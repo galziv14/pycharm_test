c8bacb35961c108f07325bd7307690e7df7089c1
Gal Ziv <galziv@yahoo.com>
2023-05-25 11:52:36 +0300

    corrected file

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 # calculator
-
 def add(n1, n2):
     return n1 + n2
 
-def substract(n1, n2):
+
+def subtract(n1, n2):
     return n1 - n2
+
 
 def multiply(n1, n2):
     return n1*n2
+
 
 def division(n1, n2):
     return n1/n2
 
 
-operations = {"+": add, "-": substract, "*": multiply, "/": division}
+operations = {"+": add, "-": subtract, "*": multiply, "/": division}
 
 
 def calculator():
